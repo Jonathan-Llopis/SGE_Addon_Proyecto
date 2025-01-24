f8682d33926f6ed497adac6e20f1a2644d97a99d
Jonathan <jonathan.llopis.linares@gmail.com>
2025-01-24 16:42:57 +0100

    Mejoras Segunda Entrega

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,5 @@ from . import gestion_zoo
 from . import gestion_zoo_animal
 from . import gestion_zoo_especie
 from . import gestion_zoo_habitats
-from . import gestion_zoo_peligro
 from . import gestion_zoo_raza
 
